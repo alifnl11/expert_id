e2e65940a740f60477bd346e04d18123f3aefc58
alifnl11 <zerpprogaming@gmail.com>
2020-11-16 22:44:58 +0700

    menu button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,21 +7,21 @@ const Header = props => {
         <header class="l-header">
           <nav class="nav bd-grid">
             <div>
-              <a href="#" class="nav_logo">SARA</a>
+              <a href="#" class="nav__logo">Expert.id</a>
             </div>
 
-            <div class="nav_menu" id="nav-menu">
+            <div class="nav__menu" id="nav-menu">
               <ul class="nav_list">
-                <li class="nav_item"><a href="#home" class="nav_link">Home</a></li>
-                <li class="nav_item"><a href="#featured" class="nav_link">Featured</a></li>
-                <li class="nav_item"><a href="#new" class="nav_link">New</a></li>
-                <li class="nav_item"><a href="#subscribed" class="nav_link">Subscribed</a></li>
+                <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                <li class="nav__item"><a href="#featured" class="nav__link">Featured</a></li>
+                <li class="nav__item"><a href="#new" class="nav__link">New</a></li>
+                <li class="nav__item"><a href="#subscribed" class="nav__link">Subscribed</a></li>
               </ul>
             </div>
 
             <div>
-                <box-icon name='cart' class="nav_cart"></box-icon>
-                <box-icon name='menu' class="nav_toggle" id="nav-toggle"></box-icon>
+                <box-icon name='cart' class="nav__cart"></box-icon>
+                <box-icon name='menu' class="nav__toggle" id="nav-toggle"></box-icon>
             </div>
           </nav>
       </header>
