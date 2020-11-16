46e03b1ed5277065460906e4dfd0790ef0db567a
alifnl11 <zerpprogaming@gmail.com>
2020-11-16 23:09:42 +0700

    home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 import Header from './components/Header';
 import './default.css';
 import './main.js';
+import homePic from './assets/img/home.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="l-main">
+        {/* HOME */}
+        <section className="home" id="home">
+          <div className="home__container bd-grid">
+            <div className="home__data">
+              <h1 className="home__tittle">NEW<br></br><span>ARRIVAL</span></h1>
+              <a href="#featured" className="button">GO SHOPPING</a>
+            </div>
+
+            <img src={homePic} alt="" className="home__img"></img>
+          </div>
+        </section>
+
+      </main>
     </div>
   );
 }
