4e1caf8ce98fd064b4a7b1548ce110832757bfc3
alifnl11 <zerpprogaming@gmail.com>
2020-11-17 23:15:16 +0700

    newsletter section

--- src/App.js
+++ src/App.js
@@ -155,6 +155,22 @@ function App() {
                 </div>
             </div>
         </section>
+
+        {/* NEWSLETTER */}
+        <section class="newsletter section" id="subscribed">
+            <div class="newsletter__container bd-grid">
+                <div class="newsletter__ subscribe">
+                    <h2 class="section-title">OUR NEWSLETTER</h2>
+                    <p class="newsletter__description">Promotion new product and special offers. Directly to your email</p>
+
+                    <form action="" class="newsletter__form">
+                        <input type="text" class="newsletter__input" placeholder="Enter your email"></input>
+                        <a href="#" class="button">SUBSCRIBE</a>
+                    </form>
+                </div>
+            </div>
+        </section>
+
       </main>
     </div>
   );
